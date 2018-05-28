319e51b7e80ee4d3cf0381fd993f51753774d2d4
shenbo <shenbo@hotmail.com>
2018-05-28 12:27:33 +0800

    update css class.

--- ximalaya.py
+++ ximalaya.py
@@ -4,58 +4,60 @@ from bs4 import BeautifulSoup
 import os
 
 
-# 冬吴同学会
+# 冬吴同学会 @ 喜马拉雅 API_url
 album_url = 'http://www.ximalaya.com/2452186/album/8475135'
+json_url = 'http://www.ximalaya.com/tracks/{}.json'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/57.0.2987.133'}
-res = requests.get(album_url, headers=headers)
-
-# 获得专辑名称
-# soup = BeautifulSoup(res.content, 'lxml')
-soup = BeautifulSoup(res.content, 'html.parser')
-print(soup.title.string)
-
-# 获得各期节目ID，生成json链接 
-mp3_ids = soup.select_one('.personal_body').attrs['sound_ids']
-print('共有{}期节目'.format(len(mp3_ids)))
-
-json_url = 'http://www.ximalaya.com/tracks/{id}.json'
-json_urls = [json_url.format(id=i) for i in mp3_ids.split(',')]
-# print(json_urls)
 
 
-def albums_list(num = 10):
-    mp3_titles = soup.find_all('a',{'class': 'title'})
-    if num > len(mp3_titles):
-        num = len(mp3_titles)
+# 获得专辑中的节目列表
+def get_mp3_list():
+    # 获得专辑名称
+    res = requests.get(album_url, headers=headers)
+    # soup = BeautifulSoup(res.content, 'lxml')
+    soup = BeautifulSoup(res.content, 'html.parser')
+    print(soup.title.string)
 
-    print('--节目清单--')
-    for link in mp3_titles[0:num]:
-        print(link.get('title'))
+    # 网页中关于节目描述的div格式
+    # <div class="e-2304105070 text">
+    #     <a title="No.0 xxxxxxxxxxx" href="/shangye/8475135/idxxxxxxxx">No.0 xxxxxxxxxx</a>
+    # </div>
+
+    # 获得节目列表
+    mp3_div = soup.find_all('div',{'class': 'e-2304105070 text'})
+    mp3_list = []
+    for div in mp3_div:
+        # 获得节目名称、ID、JSON
+        title = div.a.get('title')
+        id = div.a.get('href').split('/')[-1]
+
+        mp3_info = {'title': title, 'id': id}
+        mp3_list.append(mp3_info)
+    # print('--节目清单--\n',mp3_list)
+    return mp3_list
+
+# get_mp3_list()
 
 
-
-def get_mp3_info(js_url):
-    mp3_info = requests.get(js_url, headers=headers).json()
+# 下载单个节目
+def download_mp3(id):
+    mp3_info = requests.get(json_url.format(id), headers=headers).json()
     filename = mp3_info['title'].replace('\"', '“').replace(':', '：') + '.m4a'
     path = mp3_info['play_path']
-     
-    return filename, path
 
-
-def download(file_name, mp3_path):
     os.chdir(os.getcwd())
-    if os.path.exists(file_name):
+    if os.path.exists(filename):
         return 'Already exists'
 
     # http://stackoverflow.com/questions/13137817/how-to-download-image-using-requests
     try:
-        with open(file_name, 'wb') as f:
-            response = requests.get(mp3_path, stream=True)
+        with open(filename, 'wb') as f:
+            response = requests.get(path, stream=True)
             if not response.ok:
-                print('response error with', file_name)
+                print('response error with', filename)
 
             total_length = response.headers.get('content-length')
-            print(file_name, ', file size: ', int(total_length)/1000000.0, ' MB')
+            print(filename, ', file size: ', int(total_length)/1000000.0, ' MB')
 
             chunk_size = 1024
             for block in response.iter_content(chunk_size):
@@ -63,30 +65,33 @@ def download(file_name, mp3_path):
             print('ok ---')
 
     except Exception as e:
-        print('other error with', file_name)
-        os.remove(file_name)
+        print('other error with', filename)
+        os.remove(filename)
+
+# download_mp3('90460149')
 
 
-def download_ablum(num=3):
-    if len(json_urls) < num:
-        num = len(json_urls)
-    for i in json_urls[0:num]:
-        f, p = get_mp3_info(i)
-        download(f, p)
+# 下载专辑
+def download_ablum(num=1):
+    mp3_list = get_mp3_list()
+    if len(mp3_list) < num:
+        num = len(mp3_list)
 
+    for i in mp3_list[0:num]:
+        # print(i['id'])
+        download_mp3(i['id'])
 
-hint = '\n >> 请输入命令...\n'\
-    '    --d        下载近期节目...\n'\
-    '    --l        列出所有节目...\n'\
-    ' >> '
+download_ablum(5)
 
-
-cmd = input(hint)
-if cmd == 'q':
-    sys.exit(1)
-elif cmd == 'l':
-    albums_list(10)
-elif cmd == 'd':
-    download_ablum(5)
-else :
-    pass
+# hint = '\n >> 请输入命令...\n'\
+#     '    --d        下载近期节目...\n'\
+#     ' >> '
+#
+#
+# cmd = input(hint)
+# if cmd == 'q':
+#     sys.exit(1)
+# elif cmd == 'd':
+#     download_ablum(5)
+# else :
+#     pass
